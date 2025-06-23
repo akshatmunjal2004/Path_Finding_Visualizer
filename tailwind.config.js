4e34f13fc78db1444b3911d308a20bb82d0128a2
Akshat Munjal <akshatmunjal2004@gmail.com>
2025-06-23 14:58:17 +0530

    path visualizer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx}",
+    "./public/index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#27374D',
+        accent: '#9DB2BF',
+        highlight: '#DDE6ED',
+        wall: '#1C1F26',
+        visited: '#748DA6'
+      }
+    }
   },
   plugins: [],
 }
-
